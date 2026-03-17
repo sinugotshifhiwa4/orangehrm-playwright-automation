40bbf26dd549935aff6665b5563163d3c3b70953
Tshifhiwa Sinugo <tshifhiwa@amrod.co.za>
2026-03-17 19:05:38 +0200

    chore: add tsx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import prettierConfig from "eslint-config-prettier";
-import base from "./src/configuration/eslint/base";
-import imports from "./src/configuration/eslint/imports";
-import playwrightRules from "./src/configuration/eslint/playwright";
-import ignores from "./src/configuration/eslint/ignores";
+import base from "./src/configuration/eslint/base.ts";
+import imports from "./src/configuration/eslint/imports.ts";
+import playwrightRules from "./src/configuration/eslint/playwright.ts";
+import ignores from "./src/configuration/eslint/ignores.ts";
 
 export default [
   ...base,
